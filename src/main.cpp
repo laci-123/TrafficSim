ed5f649e4a69e6b9b1f5789b904816b8433687f0
Gohér László <goher.laci@gmail.com>
2024-09-05 11:56:45 +0200

    remove stuff used for tesing Emscripten file IO

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,6 @@ int main(int argc, char **argv) {
   int init_window_width { std::stoi(argv[1]) };
   int init_window_height{ std::stoi(argv[2]) };
 
-  std::cout << "HERE (1)" << std::endl;
-  std::ifstream file{"assets/proba.txt"};
-  if(file) {
-    std::cout << file.rdbuf() << std::endl;
-    std::cout << "HERE (2)" << std::endl;
-  }
-  else {
-    std::cout << "HERE [:-(]" << std::endl;
-  }
-  
   SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
   InitWindow(init_window_width, init_window_height, "Raylib WASM Example");
   SetWindowState(FLAG_WINDOW_RESIZABLE);
