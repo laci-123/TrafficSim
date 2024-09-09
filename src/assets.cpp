5847c19bad067ca783db4d1cb73800f1ef70fa54
Gohér László <goher.laci@gmail.com>
2024-09-09 18:04:12 +0200

    textures managed by Assets don't get unloaded prematurely

--- src/assets.cpp
+++ src/assets.cpp
@@ -4,23 +4,25 @@
 
 std::shared_ptr<Texture2D> Assets::get_texture(const char* path, int width, int height) {
   auto key = AssetKey{.path = std::string{path}, .width = width, .height = height};
+  std::shared_ptr<Texture2D> result;
+
   if(auto search = this->textures.find(key); search != this->textures.end()) {
-    this->remove_unused_textures();
-    return search->second;
+    result = search->second;
   }
   else {
     Image image = LoadImageSvg(path, width, height);
     Texture texture = LoadTextureFromImage(image);
     UnloadImage(image);
-    auto shared_texture{std::shared_ptr<Texture2D>(new Texture2D{texture},
-                                                   [](Texture2D *texture){
-                                                     UnloadTexture(*texture);
-                                                     delete texture;
-                                                   })};
-    this->textures.insert(std::make_pair(key, shared_texture));
-    this->remove_unused_textures();
-    return shared_texture;
+    result = std::shared_ptr<Texture2D>(new Texture2D{texture},
+                                        [](Texture2D *texture){
+                                          UnloadTexture(*texture);
+                                          delete texture;
+                                        });
+    this->textures.insert(std::make_pair(key, result));
   }
+
+  this->remove_unused_textures();
+  return result;
 }
 
 void Assets::remove_unused_textures() {
