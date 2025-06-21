150d47e1dfafe2247f4784ca581001edc09cf133
Gohér László <goher.laci@gmail.com>
2025-06-21 18:29:45 +0200

    setup `hecs`

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,9 +1,19 @@
 use eframe;
 use eframe::egui;
+use hecs;
+use crate::math;
 
 
 pub struct Game {
-    
+    world: hecs::World,
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        Self {
+            world: hecs::World::new(),
+        }
+    }
 }
 
 impl eframe::App for Game {
@@ -15,10 +25,31 @@ impl eframe::App for Game {
             
             egui::Frame::canvas(ui.style()).show(ui, |ui| {
                 let size = ui.available_size();
-                let (_response, painter) = ui.allocate_painter(size, egui::Sense::click_and_drag());
+                let (response, painter) = ui.allocate_painter(size, egui::Sense::click_and_drag());
 
-                painter.circle(egui::Pos2::new(100.0, 100.0), 5.0, egui::Color32::WHITE, egui::Stroke::default());
+                if response.clicked() {
+                    if let Some(mouse_pos) = response.interact_pointer_pos() {
+                        let position = Position(math::Vektor::from(mouse_pos));
+                        let velocity = Velocity(math::Vektor::<2>{ coordinates: [1.0, 1.0] });
+                        self.world.spawn((position, velocity));
+                    }
+                }
+
+                for (_id, (position, velocity)) in self.world.query_mut::<(&mut Position, &Velocity)>() {
+                    position.0 += velocity.0.clone();
+                }
+
+                for (_id, position) in self.world.query::<&Position>().iter() {
+                    painter.circle(egui::Pos2::from(position.0.clone()), 5.0, egui::Color32::WHITE, egui::Stroke::default());
+                }
             });
         });
     }
 }
+
+
+struct Position(math::Vektor<2>);
+
+
+struct Velocity(math::Vektor<2>);
+
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions::default();
     eframe::run_native("próba", options, Box::new(|_context| {
-        Ok(Box::new(game::Game{}))
+        Ok(Box::new(game::Game::default()))
     }))
 }
 
 
 mod game;
+mod math;
